8ef22f6b815b1a8ed07a3fd77b7da031b1a221f9
DrRon <piepmail@lefthandmedia.com>
2023-06-07 17:19:54 +0200

    Added Better Demon AI
    moet wel nog het in en uit bed mechanic maken

--- ProjectVrij1/Assets/01_Scripts/Philip/Demon.cs
+++ ProjectVrij1/Assets/01_Scripts/Philip/Demon.cs
@@ -2,98 +2,139 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Demon : MonoBehaviour
 {
-	[SerializeField] private enum StateEnum { Idle, Patrol, Attack }
-	[SerializeField] private StateEnum currentState;
+    private enum StateEnum { Idle, Patrol, Attack }
+    [SerializeField] private StateEnum currentState;
 	[SerializeField] private float ViewDistance = 5;
-	[SerializeField] private float CooldownTimer;
-	[SerializeField] private float CooldownStartTime;
+    [SerializeField] private float rayThreshold = 1.0f;
+    [SerializeField] private float closeDistanceThreshold = 2.0f;
+    [SerializeField] private string sceneToLoad = "GameOverScene";
+    [SerializeField] private float CooldownTimer;
+    [SerializeField] private float CooldownStartTime;
+    [SerializeField] private LayerMask playerLayerMask;
 
-	[Header("NavMeshAgent")]
-	[SerializeField] private NavMeshAgent NavMeshAgent;
-	public PatrolPoints[] PatrolPoints;
-	[SerializeField] private int patrolIndex = 0;
+    [Header("NavMeshAgent")]
+    [SerializeField] private NavMeshAgent NavMeshAgent;
+    public PatrolPoints[] PatrolPoints;
+    [SerializeField] private Transform raycastOrigin;
+    [SerializeField] private int patrolIndex = 0;
 
-	[Header("Reference Object")]
-	[SerializeField] private GameObject Player;
+    [Header("Reference Object")]
+    [SerializeField] private GameObject Player;
+    //[SerializeField] private AdultToChildMode adultToChildMode;
 
-	private void Awake()
-	{
-		NavMeshAgent = GetComponent<NavMeshAgent>();
-	}
+    private void Awake()
+    {
+        NavMeshAgent = GetComponent<NavMeshAgent>();
 
-	private void Start()
-	{
-		if (PatrolPoints.Length != 0)
-		{
-			NavMeshAgent.SetDestination(PatrolPoints[patrolIndex].transform.position);
-		}
-	}
+        if (raycastOrigin == null)
+        {
+            // Create a new GameObject as the raycast origin if not assigned
+            raycastOrigin = new GameObject("RaycastOrigin").transform;
+            raycastOrigin.SetParent(transform);
+            raycastOrigin.localPosition = Vector3.zero;
+            raycastOrigin.localRotation = Quaternion.identity;
+        }
+    }
 
-	private void Update()
-	{
-		CheckState();
-	}
+    private void Start()
+    {
+        if (PatrolPoints.Length != 0)
+        {
+            NavMeshAgent.SetDestination(PatrolPoints[patrolIndex].transform.position);
+        }
+    }
 
-	private void CheckState()
-	{
-		switch (currentState)
-		{
-			case StateEnum.Idle: IdleBehaviour(); break;
-			case StateEnum.Patrol: PatrolBehaviour(); break;
-			case StateEnum.Attack: AttackBehaviour(); break;
-		}
-	}
+    private void Update()
+    {
+        CheckState();
+    }
 
-	private void IdleBehaviour()
-	{
-		if (Vector3.Distance(transform.position, Player.transform.position) < ViewDistance)
-		{
-			currentState = StateEnum.Attack;
-		}
+    private void CheckState()
+    {
+        switch (currentState)
+        {
+            case StateEnum.Idle: IdleBehaviour(); break;
+            case StateEnum.Patrol: PatrolBehaviour(); break;
+            case StateEnum.Attack: AttackBehaviour(); break;
+        }
+    }
 
-		CooldownTimer = CooldownStartTime;
-		CooldownTimer -= 1000 * Time.deltaTime;
-		if (CooldownTimer <= 0)
-		{
-			NavMeshAgent.SetDestination(PatrolPoints[patrolIndex].transform.position);
-			currentState = StateEnum.Patrol;
-		}
-	}
+    private void IdleBehaviour()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hit, ViewDistance, playerLayerMask))
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                currentState = StateEnum.Attack;
+            }
+        }
 
-	private void PatrolBehaviour()
-	{
-		if (NavMeshAgent.remainingDistance <= NavMeshAgent.stoppingDistance)
-		{
-			patrolIndex++;
-			if (patrolIndex < PatrolPoints.Length)
-			{
-				currentState = StateEnum.Idle;
-			}
+        Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * ViewDistance, Color.red);
 
-			if (patrolIndex >= PatrolPoints.Length)
-			{
-				patrolIndex = -1;
-			}
-		}
+        CooldownTimer -= Time.deltaTime;
+        if (CooldownTimer <= 0)
+        {
+            NavMeshAgent.SetDestination(PatrolPoints[patrolIndex].transform.position);
+            currentState = StateEnum.Patrol;
+        }
+    }
 
-		if (Vector3.Distance(transform.position, Player.transform.position) < ViewDistance)
-		{
-			currentState = StateEnum.Attack;
-		}
-	}
+    private void PatrolBehaviour()
+    {
+        if (NavMeshAgent.remainingDistance <= NavMeshAgent.stoppingDistance)
+        {
+            patrolIndex++;
+            if (patrolIndex < PatrolPoints.Length)
+            {
+                currentState = StateEnum.Idle;
+            }
 
-	private void AttackBehaviour()
-	{
-		Debug.Log(" attack ");
-		NavMeshAgent.SetDestination(Player.transform.position);
-		NavMeshAgent.transform.rotation = Quaternion.LookRotation((Player.transform.position - transform.position).normalized);
+            if (patrolIndex >= PatrolPoints.Length)
+            {
+                patrolIndex = -1;
+            }
+        }
 
-		if (Vector3.Distance(transform.position, Player.transform.position) > ViewDistance * 2)
-		{
-			currentState = StateEnum.Patrol;
-		}
-	}	
+        RaycastHit hit;
+        if (Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hit, ViewDistance, playerLayerMask))
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                currentState = StateEnum.Attack;
+            }
+        }
+
+        Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * ViewDistance, Color.red);
+    }
+
+    private void AttackBehaviour()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.transform.position);
+
+        if (distanceToPlayer < closeDistanceThreshold)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+            return;
+        }
+
+        Debug.Log(" attack ");
+        NavMeshAgent.SetDestination(Player.transform.position);
+        //NavMeshAgent.transform.rotation = Quaternion.LookRotation((Player.transform.position - transform.position).normalized);
+
+        Vector3 directionToPlayer = Player.transform.position - transform.position;
+        directionToPlayer.y = 0;
+        Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+        NavMeshAgent.transform.rotation = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
+
+
+        if (distanceToPlayer > ViewDistance * 2)
+        {
+            currentState = StateEnum.Patrol;
+        }
+    }
 }
