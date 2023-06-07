0872f2d75889d9ba00e382436b653045bab29848
DrRon <piepmail@lefthandmedia.com>
2023-06-08 01:42:14 +0200

    day night cycle

---
+++ ProjectVrij1/Assets/01_Scripts/Philip/DayNightCycle.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+	[SerializeField] private Animator anim;
+	[SerializeField] private GameObject demon;
+	[SerializeField] private Demon demonScript;
+	public bool DayOrNight;
+
+	private void Awake()
+	{
+		DayOrNight = true;
+		demonScript.enabled = false;
+		demon.SetActive(false);
+	}
+
+	private void Update()
+	{
+		//day
+		if (DayOrNight)
+		{
+			anim.SetInteger("DNC", 1);
+			demonScript.enabled = false;
+			demon.SetActive(false);
+		}
+		//night
+		if (!DayOrNight)
+		{
+			anim.SetInteger("DNC", 0);
+			demonScript.enabled = true;
+			demon.SetActive(true);
+		}
+	}
+
+
+}
