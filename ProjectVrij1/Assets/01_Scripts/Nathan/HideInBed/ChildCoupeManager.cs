8e8f461824a47905b73794cebdb9f1a9b7ba1ff5
nathan <nathanhoh951@gmail.com>
2023-06-14 23:19:05 +0200

    Added Camera Switch

--- ProjectVrij1/Assets/01_Scripts/Nathan/HideInBed/ChildCoupeManager.cs
+++ ProjectVrij1/Assets/01_Scripts/Nathan/HideInBed/ChildCoupeManager.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class ChildCoupeManager : MonoBehaviour
 {
-    public ManagerDrawer DrawerManager;
-    
+    [Header("managers")]
+    public ManagerDrawer drawerManager;
+    public CameraManager cameraManager;
+    public PlayerInputManager playerInputManager;
+
+    [Header("miscellaneous")]
+    public bool IsDay;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +23,17 @@ public class ChildCoupeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerInputManager.InBed == true)
+        {
+            cameraManager.CameraBed.SetActive(true);
+            drawerManager.Player.SetActive(false);
+        }
+
+        if(playerInputManager.InBed == false)
+        {
+            drawerManager.Player.SetActive(true);
+            cameraManager.CameraBed.SetActive(false);
+        }
+        Debug.Log(playerInputManager.InBed);
     }
 }
